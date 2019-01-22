849690ca8c84c4f538e502ed5f08362ceefa3441
kittiphongTha <kittixzz@gmail.com>
2019-01-22 12:23:39 +0700

    add UI Label

--- Test_gitignoreNew/ViewController.swift
+++ Test_gitignoreNew/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HelloName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        HelloName.text = "Hello Noom"
     }
 
 
