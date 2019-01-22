15d5e3d6318f6624bf1983b92010f143112a6f64
kittiphongTha <kittixzz@gmail.com>
2019-01-22 12:04:38 +0700

    add file Xcode

---
+++ Test_gitignoreNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_gitignoreNew
+//
+//  Created by internship on 22/1/2562 BE.
+//  Copyright © 2562 SmileFOKUS private limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
