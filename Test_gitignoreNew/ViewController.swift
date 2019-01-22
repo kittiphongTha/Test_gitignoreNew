80774c48a0dd0d0a7e1f8b1313d006872dd3feb6
kittiphongTha <46708529+kittiphongTha@users.noreply.github.com>
2019-01-22 12:26:27 +0700

    Update ViewController.swift

--- Test_gitignoreNew/ViewController.swift
+++ Test_gitignoreNew/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Hello
         HelloName.text = "Hello Noom Kittiphong"
     }
 
